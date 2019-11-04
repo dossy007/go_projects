b302ab167ac9892a31a97c3f80db27b27d45b826
dossy007 <fedox.toshi@gmail.com>
2019-11-04 17:23:25 +0900

    First commit

--- http.go
+++ http.go
@@ -118,6 +118,7 @@ func main() {
     Body string
     Image string
   }
+  jkj
 
   // var post Post
 
@@ -148,7 +149,6 @@ func main() {
             log.Fatal(err)
         }
         defer rows.Close()
-    var hairetu []int
 
 for rows.Next() { //for文
     var id int
@@ -160,7 +160,6 @@ for rows.Next() { //for文
         log.Fatal(err)
     }
     fmt.Println(id, name,image)
-    nhairetu = append(hairetu,name,image)
     // fmt.Println(hai)
 }
 
@@ -215,129 +214,3 @@ for rows.Next() { //for文
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//このfileでserverは立てれている!!!
-
-package main
-
-import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
-    "strings"
-    "database/sql"
-    _"github.com/go-sql-driver/mysql"
-)
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()       //urlが渡すオプションを解析します。POSTに対してはレスポンスパケットのボディを解析します（request body）
-    //注意：もしParseFormメソッドがコールされなければ、以下でフォームのデータを取得することができません。
-    fmt.Println(r.Form) //これらのデータはサーバのプリント情報に出力されます
-    // fmt.Println("path", r.URL.Path)
-    // fmt.Println("scheme", r.URL.Scheme)
-    // fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
-    // fmt.Fprintf(w, "Hello astaxie!") //ここでwに書き込まれたものがクライアントに出力されます。
-    t,_:= template.ParseFiles("index.html")
-    // template.ParseFile関数は、edit.htmlの内容を読み込み、*template.Templateを返します。
-
-    type Mydata struct {
-        Name string
-        Age int
-        Message string
-    }
-
-    //構造体を定義 構造体で渡さないとあかんねんて(集合体)
-    pers := Mydata{
-        Name: "dossy",
-        Age: 100,
-        Message: "変数を持って来ているよ",
-    }
-
-    t.Execute(w,pers)
-    // t.Executeメソッドはテンプレートを実行し、生成したHTMLをhttp.ResponseWriterに書き出します。 ドット付きの識別子である.Titleと.Bodyは、それぞれp.Titleとp.Bodyを参照します。
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("method:", r.Method) //リクエストを取得するメソッド
-    if r.Method == "GET" {
-        //rはhttprequestでmethodはgetの時にtemplateを読む
-        t, _ := template.ParseFiles("login.gtpl.html")
-        //同じfolderのhtmlをここで探す
-        t.Execute(w, nil)
-    } else {
-        //必ず明示的にr.ParseForm()をコールした後でなければ、このフォームのデータに対して操作を行うことはできません
-        r.ParseForm()
-
-        //ログインデータがリクエストされ、ログインのロジック判断が実行されます。terminalにformの中身が表示
-        fmt.Println("username:", r.Form["username"])
-        fmt.Println("password:", r.Form["password"])
-        fmt.Println("aaa",r.Form)
-        //r.Formには全てのリクエストのデータが入っている
-        // map[pass:111,name:111]]的な感じ
-    }
-}
-
-func main() {
-    db, err := sql.Open("mysql","root:@/mydb")
-    if err != nil {
-        log.Fatal("エラーでっせ")
-    }
-    defer db.Close()
-
-
-    rows,err := db.Query(`
-        SELECT id,body,image FROM posts
-        `)
-        if err != nil{
-            log.Fatal(err)
-        }
-        defer rows.Close()
-    var hairetu []int
-
-for rows.Next() { //for文
-    var id int
-    var name string
-    var image string
-    //空の変数を定義し、Scanで値をコピー
-    //id ,body, imageの順番で取得しているので、その順番で入る
-    if err := rows.Scan(&id, &name, &image); err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(id, name,image)
-    nhairetu = append(hairetu,name,image)
-    // fmt.Println(hai)
-}
-
-sasa
-
-  //posts_tableにid 4 body marketingをinsertするsqlをresultに入れる
-    if err != nil {
-        panic(err.Error())
-    }
-
-    http.HandleFunc("/", sayhelloName)       //アクセスのルーティングを設定します
-    http.HandleFunc("/login", login)         //アクセスのルーティングを設定します /loginのroutingが走ればlogin funcが動く
-    error := http.ListenAndServe(":3000", nil) //監視するポートを設定します
-    if error != nil {
-        log.Fatal("ListenAndServe: ", error)
-    }
-}
