72b94494217dda1c114a2ef3bcf2ce4076484d6e
dossy007 <fedox.toshi@gmail.com>
2019-11-17 17:19:05 +0900

    Add mji

---
+++ moji.go
@@ -0,0 +1,51 @@
+package main
+import (
+	"fmt"
+	_"strings"
+)
+
+func add(x,y,c int)(int,int){
+	// fmt.Println("add fun")
+	// fmt.Println(x+y)
+	return (x+y)*c,x
+}
+
+
+func exit(x,y int)(result int) {
+	result = x+y
+	return result
+}
+
+func main(){
+	// add(10,20)
+	// r1,r2 := add(10,20,30)
+	// fmt.Println(r1)
+	// fmt.Println(r2)
+
+	// // fmt.Println(exit(1,22))
+	// r11 := exit(11,22)
+	// fmt.Println(r11)
+//slice
+	// a := make([]int,5)
+	// a := make([]int,0,5)
+	// for i:= 0;i<5;i++{
+	// 	a = append(a,i)
+	// 	fmt.Println(a)
+	// }
+	// fmt.Println(a)
+
+	//byte
+	// b:= []byte{72,73}
+	// fmt.Println(b)
+	// fmt.Println(string(b))
+	// c:= []byte("GI")
+	// fmt.Println(c)
+	// fmt.Println(string(c))
+
+
+
+
+	//////簡単な入力待ちを行なってrubyのtitleや感想を入れるアルゴリズムを作成し理解を深める。
+	
+
+}
