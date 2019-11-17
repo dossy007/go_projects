6e9db221c5da4013d702d38b3fc7956ea0540341
dossy007 <fedox.toshi@gmail.com>
2019-11-17 18:04:26 +0900

    Add explanation

--- range.go
+++ range.go
@@ -6,10 +6,21 @@ import (
 
 func main () {
 	sli := make([]int,0)
-	sli = append(sli,22,23,24)
-	// fmt.Println(len(sli))
+	sli = append(sli,22,23,24,25)
 
-	for num := range sli{
-		fmt.Println("番号",num,"slice-number",sli[num])
+	for num, block := range sli{
+		fmt.Println("番号",num,"slice-number",block)
 	}
+	//explanation
+	//rangeはrubyで言う所のeachみたいなもん
+	//rangeで回せるのは、配列、slice,文字列,map,channelのみ。
+	//for文でrangeを用いると配列などから要素を1つずつ取り出してroopできる。
+
+	//for num , block := range 回したい要素 {
+		// fmt.Println(num,block)
+		//numが0から要素の数分のlength
+		//blockが１つずつの要素
+	// }
+
+
 }
