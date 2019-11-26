547c4343c0b75886dcd3c5a4ead9fc52dfad1c33
dossy007 <fedox.toshi@gmail.com>
2019-11-26 18:00:37 +0900

    add form POST

--- todo/main.go
+++ todo/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handle.Showindex) //index.htmlを表示
+	// http.HandleFunc関数を使い、第1引数にパスを、第2引数に処理を指定することでリクエストを処理できるようになります。
 
 	http.HandleFunc("/new", handle.New)
 	http.HandleFunc("/create", handle.Create)
